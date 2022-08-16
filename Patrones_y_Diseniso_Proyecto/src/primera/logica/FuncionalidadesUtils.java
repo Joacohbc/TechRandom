fffd0efb85a32cf3346d960dc82f35459f0279d6
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-16 14:41:06 -0300

    Alta de funcionalidad y rol

--- Patrones_y_Diseniso_Proyecto/src/primera/logica/FuncionalidadesUtils.java
+++ Patrones_y_Diseniso_Proyecto/src/primera/logica/FuncionalidadesUtils.java
@@ -1,5 +1,29 @@
 package primera.logica;
 
+import java.sql.SQLException;
+
+import primera.Funcionalidad;
+import primera.Retorno;
+import primera.basededatos.BDUtils;
+
 public class FuncionalidadesUtils {
     
+    public static Retorno AltaDescripcion(Funcionalidad r) {
+
+        Object[] values = new Object[1];
+        values[0] = r.getNombre();
+        values[1] = r.getDescripcion();
+        
+        try {
+            BDUtils.ExecuteDML(
+                    "insert into funcionalidad values(null,?, ?))",
+                    values);
+            return new Retorno("Rol dado de alta con exito", null);
+        } catch (ClassNotFoundException e) {
+            return new Retorno("no se pudo acceder a los driver de conexion", e);
+        } catch (SQLException e) {
+            return new Retorno("no se pudo dar de alta a el rol", e);
+        }
+    }
+
 }
