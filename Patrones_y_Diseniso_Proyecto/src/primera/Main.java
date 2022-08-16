d8a091363fa52e315ed956052dc920d9fc1f3444
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-16 11:43:19 -0300

    Libreria de conexion agregada al classpath

--- Patrones_y_Diseniso_Proyecto/src/primera/Main.java
+++ Patrones_y_Diseniso_Proyecto/src/primera/Main.java
@@ -6,8 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Conexion.getConnection().close();
-		}catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("Conecatda");
+		} catch (Exception e) {
+			e.printStackTrace();
+
 		}
 	}
 }
