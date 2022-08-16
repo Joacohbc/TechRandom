cd1b054a6b01a37db65cd897df997ce6445ff184
CristopherFontes12 <107226527+CristopherFontes12@users.noreply.github.com>
2022-08-16 10:31:25 -0300

    Clases 2
    Lo guarde mal .... va de nuevo :)

--- Patrones_y_Diseniso_Proyecto/src/primera/Rol.java
+++ Patrones_y_Diseniso_Proyecto/src/primera/Rol.java
@@ -1,5 +1,63 @@
 package primera;
 
+import java.util.LinkedList;
+
 public class Rol {
-	//pru
+	private String nombre;
+	private String descripcion;
+	private LinkedList<Funcionalidad> funcionalidades;
+	
+
+	
+	
+	public Rol() {
+		super();
+	}
+
+
+
+
+	public Rol(String nombre, String descripcion, LinkedList<Funcionalidad> funcionalidades) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.funcionalidades = funcionalidades;
+	}
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public LinkedList<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+	public void setFuncionalidades(LinkedList<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Rol [nombre=" + nombre + ", descripcion=" + descripcion + ", funcionalidades=" + funcionalidades + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
