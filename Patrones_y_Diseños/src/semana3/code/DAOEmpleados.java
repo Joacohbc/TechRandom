2dba4a68876328262560a7c360bd0633ae65f025
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-22 16:07:14 -0300

    Formateo del codigo

--- Patrones_y_Diseños/src/semana3/code/DAOEmpleados.java
+++ Patrones_y_Diseños/src/semana3/code/DAOEmpleados.java
@@ -5,8 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
-
-
 public class DAOEmpleados {
 
 	private static final String ALL_EMPLEADOS = "SELECT * FROM EMPLEADOS";
@@ -14,92 +12,90 @@ public class DAOEmpleados {
 	private static final String UPDATE_EMPLEADO = "UPDATE EMPLEADOS SET  NOMBRE=?, APELLIDO=? WHERE CEDULA =?";
 	private static final String DELETE_EMPLEADO = "DELETE FROM EMPLEADOS WHERE CEDULA=?";
 	private static final String EMPLEADO_CI = "SELECT * FROM EMPLEADOS WHERE CEDULA=?";
-	
-	
-	public static boolean insert (Empleado empleado){
+
+	public static boolean insert(Empleado empleado) {
 		try {
-			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(INSERTL_EMPLEADO);
-		
+			PreparedStatement statement = BDManager.getConnection().prepareStatement(INSERTL_EMPLEADO);
+
 			statement.setString(1, empleado.getCedula());
 			statement.setString(2, empleado.getNombre());
 			statement.setString(3, empleado.getApellido());
-		
-			
+
 			int retorno = statement.executeUpdate();
-			
-			return retorno>0;
-			
+
+			return retorno > 0;
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 	}
-	
-	public static boolean update (Empleado empleado){
+
+	public static boolean update(Empleado empleado) {
 		try {
-			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(UPDATE_EMPLEADO);
-			
+			PreparedStatement statement = BDManager.getConnection().prepareStatement(UPDATE_EMPLEADO);
+
 			statement.setString(1, empleado.getNombre());
 			statement.setString(2, empleado.getApellido());
 			statement.setString(3, empleado.getCedula());
 			int retorno = statement.executeUpdate();
-			
-			return retorno>0;
-			
+
+			return retorno > 0;
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 	}
 
-	public static boolean delete(Empleado empleado){
+	public static boolean delete(Empleado empleado) {
 		try {
-			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(DELETE_EMPLEADO);
+			PreparedStatement statement = BDManager.getConnection().prepareStatement(DELETE_EMPLEADO);
 			statement.setString(1, empleado.getCedula());
-			
+
 			int retorno = statement.executeUpdate();
-			return retorno>0;
-			
+			return retorno > 0;
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
-		}	
-		
+		}
+
 	}
-	
-	public static LinkedList<Empleado> findAll(){
+
+	public static LinkedList<Empleado> findAll() {
 		LinkedList<Empleado> empleados = new LinkedList<>();
 		try {
-			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(ALL_EMPLEADOS);
+			PreparedStatement statement = BDManager.getConnection().prepareStatement(ALL_EMPLEADOS);
 			ResultSet resultado = statement.executeQuery();
-			while(resultado.next()){
+			while (resultado.next()) {
 				Empleado empleado = getEmpleadoFromResultSet(resultado);
 				empleados.add(empleado);
 			}
-			
+
 			return empleados;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
-			
+
 	}
 
-	public static Empleado find(String ciEmpleado){
+	public static Empleado find(String ciEmpleado) {
 		try {
-			PreparedStatement statement = DatabaseManager.getConnection().prepareStatement(EMPLEADO_CI);
+			PreparedStatement statement = BDManager.getConnection().prepareStatement(EMPLEADO_CI);
 			statement.setString(1, ciEmpleado);
-			
+
 			ResultSet resultado = statement.executeQuery();
 			Empleado empleado = null;
-			if(resultado.next()){
-				empleado= getEmpleadoFromResultSet(resultado);
+			if (resultado.next()) {
+				empleado = getEmpleadoFromResultSet(resultado);
 			}
 			return empleado;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
@@ -107,18 +103,15 @@ public class DAOEmpleados {
 	}
 
 	private static Empleado getEmpleadoFromResultSet(ResultSet resultado) throws SQLException {
-		
+
 		String nombre = resultado.getString("NOMBRE");
 		String apellido = resultado.getString("APELLIDO");
-		String  cedula = resultado.getString("CEDULA");
-		
-		Empleado empleado = new Empleado(cedula,nombre, apellido);
-		
+		String cedula = resultado.getString("CEDULA");
+
+		Empleado empleado = new Empleado(cedula, nombre, apellido);
+
 		return empleado;
-	
+
 	}
-	
 
-	
 }
-
--- Patrones_y_Diseños/src/semana3/code/MostrarTodo.java
+++ Patrones_y_Diseños/src/semana3/code/MostrarTodo.java
@@ -9,67 +9,62 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.util.LinkedList;
 
-//clase que muestra el contenido de una tabla de la BD utilizando una tabla
+// Clase que muestra el contenido de una tabla de la BD utilizando una tabla
 public class MostrarTodo extends JFrame {
 
-	public void mostrar(){
+	public void mostrar() {
 
-		JFrame frame =new JFrame("Mostrar");
+		JFrame frame = new JFrame("Mostrar");
 
 		// creamos el modelo de Tabla
-		DefaultTableModel modelo= new DefaultTableModel();
+		DefaultTableModel modelo = new DefaultTableModel();
 
 		// se crea la Tabla con el modelo DefaultTableModel
 		final JTable table = new JTable(modelo);
-		//crea un array que contiene los nombre de las columnas
-		final String[] columnNames = {"Nombre","Apellido","Cedula"};
+		// crea un array que contiene los nombre de las columnas
+		final String[] columnNames = { "Nombre", "Apellido", "Cedula" };
 		// insertamos las columnas
-		for(int column = 0; column < columnNames.length; column++){
-			//agrega las columnas a la tabla
+		for (int column = 0; column < columnNames.length; column++) {
+			// agrega las columnas a la tabla
 			modelo.addColumn(columnNames[column]);
 		}
 
+		// Se crea un array que ser� una de las filas de la tabla.
+		Object[] fila = new Object[columnNames.length];
+		// Se rellena cada posici�n del array con una de las columnas de la tabla en
+		// base de datos.
+		LinkedList<Empleado> todosEmpleados = DAOEmpleados.findAll();
 
-		
-			// Se crea un array que ser� una de las filas de la tabla. 
-			Object [] fila = new Object[columnNames.length]; 
-			// Se rellena cada posici�n del array con una de las columnas de la tabla en base de datos.
-					LinkedList<Empleado> todosEmpleados = DAOEmpleados.findAll();
-				
-				
-			for (int i=0;i<todosEmpleados.size();i++){
-		
-				String nombre=todosEmpleados.get(i).getNombre();
-				String apellido=todosEmpleados.get(i).getApellido();
-				String cedula=todosEmpleados.get(i).getCedula();
-			
-				fila[0] = nombre;
-				fila[1] = apellido;
-				fila[2] = cedula;
-				modelo.addRow(fila); 
-			}
-			// Se a�ade al modelo la fila completa.
-				
-			//System.out.println(modelo);
-		
+		for (int i = 0; i < todosEmpleados.size(); i++) {
 
-		//se define el tama�o de la tabla
+			String nombre = todosEmpleados.get(i).getNombre();
+			String apellido = todosEmpleados.get(i).getApellido();
+			String cedula = todosEmpleados.get(i).getCedula();
+
+			fila[0] = nombre;
+			fila[1] = apellido;
+			fila[2] = cedula;
+			modelo.addRow(fila);
+		}
+		// Se a�ade al modelo la fila completa.
+
+		// System.out.println(modelo);
+
+		// se define el tama�o de la tabla
 		table.setPreferredScrollableViewportSize(new Dimension(600, 100));
-		//Creamos un JscrollPane y le agregamos la JTable
+		// Creamos un JscrollPane y le agregamos la JTable
 		JScrollPane scrollPane = new JScrollPane(table);
-		//crea el panel
+		// crea el panel
 		JPanel panel = new JPanel();
-		//definimos un layout
-		//Agregamos el JScrollPane al contenedor
-		panel.add(scrollPane);		
+		// definimos un layout
+		// Agregamos el JScrollPane al contenedor
+		panel.add(scrollPane);
 
 		frame.add(panel);
 		frame.pack();
 
 		frame.setVisible(true);
 
-
 	}
-
 
 }
