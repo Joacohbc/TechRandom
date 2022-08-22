d616de403f2ca467be5e66d5c8445fdfda769d14
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-22 16:05:28 -0300

    Tarea Grupal de semana 3

---
+++ Patrones_y_Diseños/src/semana3/code/DatabaseManager.java
@@ -0,0 +1,32 @@
+package semana3.code;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Locale;
+
+public class DatabaseManager {
+	
+	private static Connection databaseConnection;
+	
+	private static String Connection_String = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static String usuario = "system";
+	private static String contrasenia = "12345";
+	
+	
+	static{
+		try {
+			Locale.setDefault(new Locale("es","ES"));
+			databaseConnection = DriverManager.getConnection(Connection_String, usuario, contrasenia);
+			
+		} catch (SQLException e) {
+			System.out.println("Error al conectarse a la Base de datos");
+			System.out.println(e.getErrorCode());
+		}
+	}
+	
+	public static Connection getConnection(){
+		return databaseConnection;
+	}
+
+}
