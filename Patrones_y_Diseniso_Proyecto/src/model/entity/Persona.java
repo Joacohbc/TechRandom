3cb3601bda1212046e983c2fa2a7513b832fe8ed
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-24 20:38:53 -0300

    Nuevo método en Persona
    - Para obtener la fecha de nacimiento de la persona en un String valido para darla de alta en la base de datos

--- Patrones_y_Diseniso_Proyecto/src/model/entity/Persona.java
+++ Patrones_y_Diseniso_Proyecto/src/model/entity/Persona.java
@@ -1,6 +1,7 @@
 package model.entity;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Persona {
 
@@ -13,7 +14,7 @@ public class Persona {
 	private String clave;
 	private String mail;
 	private Rol rol;
-	
+
 	public Persona() {
 		super();
 	}
@@ -35,70 +36,84 @@ public class Persona {
 	public String getDocumento() {
 		return documento;
 	}
+
 	public void setDocumento(String documento) {
 		this.documento = documento;
 	}
+
 	public String getApellido1() {
 		return apellido1;
 	}
+
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
+
 	public String getApellido2() {
 		return apellido2;
 	}
+
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
+
 	public String getNombre1() {
 		return nombre1;
 	}
+
 	public void setNombre1(String nombre1) {
 		this.nombre1 = nombre1;
 	}
+
 	public String getNombre2() {
 		return nombre2;
 	}
+
 	public void setNombre2(String nombre2) {
 		this.nombre2 = nombre2;
 	}
+
 	public LocalDate getFechaNacimiento() {
 		return fechaNacimiento;
 	}
+
+	public String getFechaNacimientoString() {
+		return fechaNacimiento.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	}
+	
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
+
 	public String getClave() {
 		return clave;
 	}
+
 	public void setClave(String clave) {
 		this.clave = clave;
 	}
+
 	public String getMail() {
 		return mail;
 	}
+
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
+
 	public Rol getRol() {
 		return rol;
 	}
+
 	public void setRol(Rol rol) {
 		this.rol = rol;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Persona [documento=" + documento + ", apellido1=" + apellido1 + ", apellido2=" + apellido2
 				+ ", nombre1=" + nombre1 + ", nombre2=" + nombre2 + ", fechaNacimiento=" + fechaNacimiento + ", clave="
 				+ clave + ", mail=" + mail + ", role=" + rol + "]";
-	} 
-	
-	
+	}
 
-	
-	
-	
-	
 }
