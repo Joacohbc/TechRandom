4adf14a9d0d3a624b0f885e1d7167fbea67dea0a
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-10-06 15:56:24 -0300

    Cambio de lugar de carpeta (estaba mal colocada

--- AE/ActividadVCSemana2Cliente/src/ejemplo/Principal.java
+++ AE/ActividadVCSemana2Cliente/src/ejemplo/Principal.java
@@ -8,30 +8,30 @@ import javax.naming.NamingException;
 import com.servicios.CarritoDeComprasBeanRemote;
 import com.servicios.GestionComprasBeanRemote;
 
-
 public class Principal {
 
 	private static void listar(List<String> carrito) {
 		String productos = "Carrito de Productos:\n";
-		for (String compra : carrito){
-			productos += "- "+compra+"\n";
+		for (String compra : carrito) {
+			productos += "- " + compra + "\n";
 		}
-		productos += "Cantidad de productos: "+carrito.size();
+		productos += "Cantidad de productos: " + carrito.size();
 		System.out.println(productos);
 	}
-	
+
 	public static void main(String[] args) throws NamingException {
-		
+
 		// Invocamos los Beans de Carrito y Compra
-		CarritoDeComprasBeanRemote carritoBean = (CarritoDeComprasBeanRemote) 
-				InitialContext.doLookup("/ActividadVCSemana2Beans/CarritoDeComprasBean!com.servicios.CarritoDeComprasBeanRemote");
-		
-		GestionComprasBeanRemote gestionBean = (GestionComprasBeanRemote)
-				InitialContext.doLookup("/ActividadVCSemana2Beans/GestionComprasBean!com.servicios.GestionComprasBeanRemote");
-		
+		CarritoDeComprasBeanRemote carritoBean = (CarritoDeComprasBeanRemote) InitialContext
+				.doLookup("/ActividadVCSemana2Beans/CarritoDeComprasBean!com.servicios.CarritoDeComprasBeanRemote");
+
+		GestionComprasBeanRemote gestionBean = (GestionComprasBeanRemote) InitialContext
+				.doLookup("/ActividadVCSemana2Beans/GestionComprasBean!com.servicios.GestionComprasBeanRemote");
+
 		// Problema 1
-		// Nos tira un error al tratar user Clases de Java noramles en metodos de los EJBs
-		
+		// Nos tira un error al tratar user Clases de Java noramles en metodos de los
+		// EJBs
+
 //		carritoBean.addCompra(new Compra("iPad Pro",550));
 //		carritoBean.addCompra(new Compra("iPhone 14 Pro",1550));
 //		carritoBean.addCompra(new Compra("Macb¿Book Pro",3000));
@@ -42,11 +42,11 @@ public class Principal {
 		carritoBean.addCompra("iPhone 14 Pro");
 		carritoBean.addCompra("MacBook Pro");
 		carritoBean.addCompra("MacBook M1 Pro");
-		
+
 		// Listamos el carrito
 		listar(carritoBean.listarCompras());
 		System.out.println();
-		
+
 		// Mostramos las estadisticas
 		System.out.println(gestionBean.mostrarEstadistica());
 		System.out.println();
@@ -54,12 +54,12 @@ public class Principal {
 		// Generamos la compra
 		System.out.println(gestionBean.comprar(carritoBean.listarCompras()));
 		System.out.println();
-		
+
 		// Borramos un elemento del carrito
 		carritoBean.removeCompra("iPad Pro");
 		listar(carritoBean.listarCompras());
 		System.out.println();
-		
+
 		// Problema 2
 		// Si pedimos los atributos mediante su acceso public no retorna los vaores
 		// Solo funciona con Getters
