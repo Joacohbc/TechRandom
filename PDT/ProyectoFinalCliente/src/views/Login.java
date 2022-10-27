3fd2f393a0455ca6260bc76438b3d30f68fd1dfb
BrunoGonzalez17 <90582466+BrunoGonzalez17@users.noreply.github.com>
2022-10-27 09:25:44 -0300

    login
    cambios en login

--- PDT/ProyectoFinalCliente/src/views/Login.java
+++ PDT/ProyectoFinalCliente/src/views/Login.java
@@ -1,10 +1,16 @@
 package views;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import com.entities.Analista;
@@ -13,21 +19,14 @@ import com.entities.Tutor;
 
 import beans.BeanIntances;
 import components.Roles;
-
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.JPasswordField;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import components.VTextBox;
+import components.ValidationObject;
+import validation.Validaciones;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtUsuario;
-	private JPasswordField txtPassword;
+	private JPasswordField textContraseña;
 
 	/**
 	 * Launch the application.
@@ -56,11 +55,6 @@ public class Login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(169, 95, 96, 19);
-		contentPane.add(txtUsuario);
-		txtUsuario.setColumns(10);
-		
 		JComboBox <Roles> comboRol = new JComboBox <Roles>();
 		comboRol.setBounds(169, 64, 164, 21);
 		contentPane.add(comboRol);
@@ -68,20 +62,20 @@ public class Login extends JFrame {
 			comboRol.addItem(rol);
 		}
 		
-		JButton btnLogin = new JButton("Login");
+		/*JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (comboRol.getSelectedItem() == Roles.ANALISTA) {
 					System.out.println(BeanIntances.user().login(txtUsuario.getText(), String.valueOf(txtPassword.getPassword()), Analista.class));
-				}else if (comboRol.getSelectedItem() == Roles.TUTOR) {
-					System.out.println(BeanIntances.user().login(txtUsuario.getText(), String.valueOf(txtPassword.getPassword()), Tutor.class));
+				}else if (comboRol.getSelectedItem() == Roles.TUTOR && textBox.isValid()) {
+					System.out.println(BeanIntances.user().login(textBox.getText(), String.valueOf(txtPassword.getPassword()), Tutor.class));
 				}else {
 					System.out.println(BeanIntances.user().login(txtUsuario.getText(), String.valueOf(txtPassword.getPassword()), Estudiante.class));
 				}
 			}
 		});
 		btnLogin.setBounds(264, 168, 102, 21);
-		contentPane.add(btnLogin);
+		contentPane.add(btnLogin);*/
 		
 		JButton btnRegistrarse = new JButton("Registrarse");
 		btnRegistrarse.addActionListener(new ActionListener() {
@@ -90,10 +84,6 @@ public class Login extends JFrame {
 		});
 		btnRegistrarse.setBounds(264, 209, 102, 21);
 		contentPane.add(btnRegistrarse);
-		
-		txtPassword = new JPasswordField();
-		txtPassword.setBounds(169, 131, 95, 19);
-		contentPane.add(txtPassword);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
 		lblNewLabel.setBounds(57, 98, 102, 13);
@@ -106,5 +96,33 @@ public class Login extends JFrame {
 		JLabel lblNewLabel_2 = new JLabel("Rol");
 		lblNewLabel_2.setBounds(57, 68, 102, 13);
 		contentPane.add(lblNewLabel_2);
+		
+		VTextBox textboxUsuario = new VTextBox();
+		textboxUsuario.setBounds(169, 95, 164, 21);
+		contentPane.add(textboxUsuario);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (comboRol.getSelectedItem() == Roles.ANALISTA && textboxUsuario.isValid()) {
+					System.out.println(BeanIntances.user().login(textboxUsuario.getText(), String.valueOf(textContraseña.getPassword()), Analista.class));
+				}else if (comboRol.getSelectedItem() == Roles.TUTOR && textboxUsuario.isValid()) {
+					System.out.println(BeanIntances.user().login(textboxUsuario.getText(), String.valueOf(textContraseña.getPassword()), Tutor.class));
+				}else if (comboRol.getSelectedItem() == Roles.ESTUDIANTE && textboxUsuario.isValid()){
+					System.out.println(BeanIntances.user().login(textboxUsuario.getText(), String.valueOf(textContraseña.getPassword()), Estudiante.class));
+					}
+				}
+		});
+		btnLogin.setBounds(264, 171, 102, 21);
+		contentPane.add(btnLogin);
+		
+		textContraseña = new JPasswordField();
+		textContraseña.setBounds(169, 126, 164, 21);
+		contentPane.add(textContraseña);
+		
 	}
 }
