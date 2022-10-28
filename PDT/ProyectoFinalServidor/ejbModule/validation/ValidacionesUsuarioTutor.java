6cbf434cf0607f0086e766865a49a0d13ea75f79
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-10-28 20:50:17 -0300

    Corrección en validaciones de Tutor

--- PDT/ProyectoFinalServidor/ejbModule/validation/ValidacionesUsuarioTutor.java
+++ PDT/ProyectoFinalServidor/ejbModule/validation/ValidacionesUsuarioTutor.java
@@ -3,6 +3,8 @@ package validation;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import com.entities.enums.TipoTutor;
+
 /**
  * Session Bean implementation class ValidacionesUsuarioTutor
  */
@@ -17,33 +19,23 @@ public class ValidacionesUsuarioTutor {
         // TODO Auto-generated constructor stub
     }
     
-    public static ValidationObject ValidarTipo (String tipo) {
-    		
-    		if (tipo != null) {
-				
-    			return ValidationObject.VALID;
-			}else {
-				return new ValidationObject("El tipo de Tutor es obligatorio");
-			}
-			
+    public static ValidationObject ValidarTipo (TipoTutor tipo) {
+		if (tipo != null) {
+			return ValidationObject.VALID;
+		} else {
+			return new ValidationObject("El tipo de Tutor es obligatorio");
+		}
     }
-    
     
     public static ValidationObject ValidarArea (String area) {
     	
     	if (Validaciones.ValidarNumerosYLetras(area, false)) {
-			
     		if (area.length() <= 50) {
-    			
     			return ValidationObject.VALID;
-				
 			}else {
-				
-				return new ValidationObject("Area deben tener un maximo de 100 caracteres");						
+				return new ValidationObject("El Area deben tener un maximo de 50 caracteres");						
 			}
-    		
 		}else {
-			
 			return new ValidationObject("El Area solo puede contener letras y numeros");
 		}
     	
