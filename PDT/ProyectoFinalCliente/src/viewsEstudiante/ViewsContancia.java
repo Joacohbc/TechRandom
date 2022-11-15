39e79318748de6e90b459416917481a81f82d558
CristopherFontes12 <cristopher.fontes@estudiantes.utec.edu.uy>
2022-11-15 20:10:42 -0300

    Se genero la tabla donde va a estar la informacion de los eventos.

--- PDT/ProyectoFinalCliente/src/viewsEstudiante/ViewsContancia.java
+++ PDT/ProyectoFinalCliente/src/viewsEstudiante/ViewsContancia.java
@@ -6,10 +6,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class ViewsContancia extends JPanel {
 
 	private JPanel contentPane;
+	private JTable tableConstancia;
 
 	/**
 	 * Launch the application.
@@ -31,14 +35,46 @@ public class ViewsContancia extends JPanel {
 	 * Create the frame.
 	 */
 	public ViewsContancia() {
-		setBounds(100, 100, 649, 582);
-
+		setBounds(100, 100, 679, 624);
+		setLayout(null);
 		
-		JButton btnNewButton = new JButton("Funciona papu 2");
-		btnNewButton.setBounds(310, 231, 103, 23);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(29, 338, 623, 192);
+		add(scrollPane);
+		
+		tableConstancia = new JTable();
+		tableConstancia.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, "", "", null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"Titulo", "Fecha de Inicio", "Fecha de fin", "Modalidad", "Localizacion", "Solicitar Constancia"
+			}
+		));
+		tableConstancia.getColumnModel().getColumn(1).setPreferredWidth(93);
+		tableConstancia.getColumnModel().getColumn(5).setPreferredWidth(115);
+		scrollPane.setViewportView(tableConstancia);
+		
+		JButton btnNewButton = new JButton("Solicitar");
+		btnNewButton.setBounds(511, 543, 89, 23);
 		add(btnNewButton);
 		
 
 	}
-
+	
+	public void cargarEventos() {
+		
+		
+	}
+	
+	
 }
