4b4c563a96b1b6a7decf8681c962f9f14535b3bb
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-11-24 19:53:06 -0300

    Opción de Mensaje de Confirmación corregida

--- PDT/ProyectoFinalCliente/src/swingutils/Mensajes.java
+++ PDT/ProyectoFinalCliente/src/swingutils/Mensajes.java
@@ -4,12 +4,15 @@ import javax.swing.JOptionPane;
 
 public class Mensajes {
 	
+	public static final int OPCION_SI= JOptionPane.YES_OPTION;
+	public static final int OPCION_NO = JOptionPane.NO_OPTION;
+
 	public static void MostrarError(String msg) {
 		JOptionPane.showMessageDialog(null, msg, "Operacion fallida", JOptionPane.ERROR_MESSAGE);
 	}
 
-	public static void MostrarSioNo(String msg) {
-		JOptionPane.showConfirmDialog(null, msg, "Confirmacion", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
+	public static int MostrarSioNo(String msg) {
+		return JOptionPane.showConfirmDialog(null, msg, "Confirmacion", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
 	}
 	
 	public static void MostrarAdvertencia(String msg) {
