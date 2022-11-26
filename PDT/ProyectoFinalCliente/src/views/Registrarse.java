d66f969aca535458e2f6455f371c9f4e67d4d09f
william-lti <106108186+william-lti@users.noreply.github.com>
2022-11-26 18:00:53 -0300

    regresar al login agregado en registrarse

--- PDT/ProyectoFinalCliente/src/views/Registrarse.java
+++ PDT/ProyectoFinalCliente/src/views/Registrarse.java
@@ -89,9 +89,16 @@ public class Registrarse extends JFrame {
 					comboItr.addItem(itr);
 				}
 			}
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				Login login = new Login();
+				login.setVisible(true);
+
+			}
 		});
 		setTitle("Registrarse");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 706, 543);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
